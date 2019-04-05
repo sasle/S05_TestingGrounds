4922754185cb9e0a6d1808e1d78564fab07987fa
Alexandre <sasle57@gmail.com>
2019-04-05 19:16:52 -0300

    Aula 256

--- TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGrounds.init.gen.cpp
+++ TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGrounds.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeTestingGrounds_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/TestingGrounds",
 				PKG_CompiledIn | 0x00000000,
-				0x1637C0F8,
-				0xA45DACD1,
+				0xB18626DF,
+				0x656BAF59,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};

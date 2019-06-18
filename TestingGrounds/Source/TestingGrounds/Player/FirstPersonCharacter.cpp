dd6d0fe2b429f819c14325a36108a67b46da0f72
Alexandre <sasle57@gmail.com>
2019-06-18 19:17:49 -0300

    Child Actor Components

--- TestingGrounds/Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ TestingGrounds/Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -93,6 +93,7 @@ void AFirstPersonCharacter::BeginPlay()
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
 	if (EnableTouchscreenMovement(InputComponent) == false) {
 		InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 	}

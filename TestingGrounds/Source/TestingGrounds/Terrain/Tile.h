ebc5703077e6099e77ca53510556fdbef5d859ab
Alexandre <sasle57@gmail.com>
2019-08-18 17:42:56 -0300

    Spawning AI from C++

--- TestingGrounds/Source/TestingGrounds/Terrain/Tile.h
+++ TestingGrounds/Source/TestingGrounds/Terrain/Tile.h
@@ -27,9 +27,11 @@ public:
 	// Sets default values for this actor's properties
 	ATile();
 
-	UFUNCTION(BlueprintCallable, Category = "Setup")
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
 	void  PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float Radius = 500, float MinScale=1, float MaxScale=1);
 
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+		void  PlaceAIPawns(TSubclassOf<APawn> ToSpawn, int MinSpawn = 1, int MaxSpawn = 1, float Radius = 500);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -57,6 +59,7 @@ private:
 	bool FindEmptyLocation(FVector& OutLocation, float Radius);
 
 	void PlaceActor(TSubclassOf<AActor> ToSpawn, const FSpawnPosition& SpawnPosition);
+	void PlaceAIPawn(TSubclassOf<APawn> ToSpawn, FSpawnPosition SpawnPosition);
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
 	UActorPool* Pool;
